88675c9d047b7b5272db66b24fafa3733d45886e
asachse1 <42878671+asachse1@users.noreply.github.com>
2020-12-09 02:32:51 -0500

    Actual Array/Data Commit
    Didn't save the file last commit.

--- Socket Client Testing.py
+++ Socket Client Testing.py
@@ -14,26 +14,30 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+    boardArray = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
 
 #Server IP
 HOST = str(sys.argv[1])
 PORT = 13037
-message = '|-----|-----|-----|\n |  X  |  X  |  X  |\n |-----|-----|-----|\n |  X  |  X  |  X  |\n |-----|-----|-----|\n |  X  |  X  |  X  |\n |-----|-----|-----|\n'
+message = "|-----|-----|-----|\n |  {0}  |  {1}  |  {2}  |\n |-----|-----|-----|\n |  {3}  |  {4}  |  {5}  |\n |-----|-----|-----|\n |  {6}  |  {7}  |  {8}  |\n |-----|-----|-----|\n".format(*boardArray)
 messageB = bytes(message, 'utf-8')
+
 #Create Socket Object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Connect to the Server and start the Three-Way Handshake
 s.connect((HOST,PORT))
 
-#Sends Data to the Server for Serverside .recv
+#Sends Data to the Server for Serverside .recv in byte format
 s.sendall(messageB)
 
 #Holds program for the serverside .sendall
 data = s.recv(1024)
-#Prints the data on Serverside???
+
+#Decodes from bytes
 tempString = data.decode("utf-8")
 
+#prints decoded data
 print('Received: \n', tempString)
 
 
